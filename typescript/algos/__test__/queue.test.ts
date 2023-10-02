e2b5cd50565814903bdd960b95b2926fb62818f5
unimafy <nima.yonten1729@gmail.com>
2023-10-03 00:06:17 +0600

    use bun test module instead

--- typescript/algos/__test__/queue.test.ts
+++ typescript/algos/__test__/queue.test.ts
@@ -1,41 +1,41 @@
-import Queue from '../queue';
+import { expect, test } from "bun:test";
+import Queue from "../queue";
 
-describe('Queue', () => {
-  let queue = new Queue<number>();
+let queue = new Queue<number>();
 
-  beforeEach(() => {
-    queue = new Queue();
-  });
+test("Queue enqueue adds an item to the queue", () => {
+  queue = new Queue();
+  queue.enqueue(1);
+  expect(queue.length).toBe(1);
+  expect(queue.peek()).toBe(1);
+});
 
-  test('enqueue adds an item to the queue', () => {
-    queue.enqueue(1);
-    expect(queue.length).toBe(1);
-    expect(queue.peek()).toBe(1);
-  });
+test("Queue deque removes an item from the queue", () => {
+  queue = new Queue();
+  queue.enqueue(1);
+  queue.enqueue(2);
+  const dequeuedItem = queue.deque();
+  expect(dequeuedItem).toBe(1);
+  expect(queue.length).toBe(1);
+});
 
-  test('deque removes an item from the queue', () => {
-    queue.enqueue(1);
-    queue.enqueue(2);
-    const dequeuedItem = queue.deque();
-    expect(dequeuedItem).toBe(1);
-    expect(queue.length).toBe(1);
-  });
+test("Queue deque returns undefined on an empty queue", () => {
+  queue = new Queue();
+  const dequeuedItem = queue.deque();
+  expect(dequeuedItem).toBeUndefined();
+});
 
-  test('deque returns undefined on an empty queue', () => {
-    const dequeuedItem = queue.deque();
-    expect(dequeuedItem).toBeUndefined();
-  });
+test("Queue peek returns the first item without removing it", () => {
+  queue = new Queue();
+  queue.enqueue(1);
+  queue.enqueue(2);
+  const peekedItem = queue.peek();
+  expect(peekedItem).toBe(1);
+  expect(queue.length).toBe(2);
+});
 
-  test('peek returns the first item without removing it', () => {
-    queue.enqueue(1);
-    queue.enqueue(2);
-    const peekedItem = queue.peek();
-    expect(peekedItem).toBe(1);
-    expect(queue.length).toBe(2);
-  });
-
-  test('peek returns undefined on an empty queue', () => {
-    const peekedItem = queue.peek();
-    expect(peekedItem).toBeUndefined();
-  });
+test("Queue peek returns undefined on an empty queue", () => {
+  queue = new Queue();
+  const peekedItem = queue.peek();
+  expect(peekedItem).toBeUndefined();
 });
